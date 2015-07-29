b127d9198cf220b275550d2bf72a648caef1a37d
ShaunDesktop <ralem1234@yahoo.com>
2015-07-28 22:01:58 -0500

    raer

--- app/src/main/java/com/example/ralemproductions/trolltext/MainActivity.java
+++ app/src/main/java/com/example/ralemproductions/trolltext/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    //test
+    //testasdf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
