e27004ddfc512d511ee254f2ca8d314d02760be3
joshtrance <joshtrance@gmail.com>
2015-07-28 21:56:47 -0500

    sms

--- app/src/main/java/com/example/ralemproductions/trolltext/Texting.java
+++ app/src/main/java/com/example/ralemproductions/trolltext/Texting.java
@@ -1,38 +1,137 @@
 package com.example.ralemproductions.trolltext;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class Texting extends ActionBarActivity {
 
+import java.util.ArrayList;
+
+
+public class Texting extends Activity implements OnItemClickListener {
+
+    private static Texting inst;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView smsListView;
+    ArrayAdapter arrayAdapter;
+
+    public static Texting instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texting);
+        smsListView = (ListView) findViewById(R.id.SMSList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        smsListView.setAdapter(arrayAdapter);
+        smsListView.setOnItemClickListener(this);
+
+
+        refreshSmsInbox();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_texting, menu);
-        return true;
+    public void refreshSmsInbox() {
+        Uri inboxURI = Uri.parse("content://sms/inbox");
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor smsInboxCursor = contentResolver.query(inboxURI, null, null, null, null);
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
+            arrayAdapter.add(str);
+        } while (smsInboxCursor.moveToNext());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void updateList(final String smsMessage) {
+        arrayAdapter.insert(smsMessage, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+        try {
+            String[] smsMessages = smsMessagesList.get(pos).split("\n");
+            String address = smsMessages[0];
+            String smsMessage = "";
+            for (int i = 1; i < smsMessages.length; ++i) {
+                smsMessage += smsMessages[i];
+            }
+
+            String smsMessageStr = address + "\n";
+            smsMessageStr += smsMessage;
+            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void Button1(View view){
+        sendSMSMessage("Yes");
+
+
+    }
+    public void Button2(View view){
+        sendSMSMessage("No");
+
+
+    }
+    public void Button3(View view){
+        sendSMSMessage("Call me");
+
+
+    }
+    public void Button4(View view){
+        sendSMSMessage("Thank you");
+
+
+    }
+    public void Button5(View view){
+        sendSMSMessage("Give me 15");
+
+
+    }
+    public void Button6(View view){
+        sendSMSMessage("HELP!");
+
+
+    }
+    protected void sendSMSMessage(String newMessage) {
+
+
+        String message = newMessage;
+        Intent intent = getIntent();
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage((intent.getStringExtra("message")), null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
+
+
+
 }
